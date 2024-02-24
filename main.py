71f8177ec704b197d7d33e1c9e2360ec462270f1
petermdtran246 <petermdtran246@gmail.com>
2024-02-24 10:35:46 -0600

    New

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 input_month = str(input())
 input_day = int(input())
 
-'''Check the Winter season'''
+#Check the Winter season
 
 if (input_month == 'January') and (1 <= input_day <= 31):
     print('Winter')
@@ -11,6 +11,8 @@ elif (input_month == "March") and (1 <= input_day <= 19):
     print('Winter')
 elif (input_month == "December") and (1 <= input_day <= 31):
     print('Winter')
+
+#Check the Spring season
 elif (input_month == 'March') and (20 <= input_day <= 31):
     print('Spring')
 elif (input_month == "April") and (1 <= input_day <= 30):
@@ -19,6 +21,8 @@ elif (input_month == "May") and (1 <= input_day <= 31):
     print('Spring')
 elif (input_month == "June") and (1 <= input_day <= 21):
     print('Spring')
+
+#Check the Summer season
 elif (input_month == 'June') and (21 <= input_day <= 30):
     print('Summer')
 elif (input_month == "July") and (1 <= input_day <= 31):
@@ -27,6 +31,8 @@ elif (input_month == "August") and (1 <= input_day <= 30):
     print('Summer')
 elif (input_month == "September") and (1 <= input_day <= 21):
     print('Summer')
+
+#Check the Autumn season.
 elif (input_month == 'September') and (22 <= input_day <= 30):
     print('Autumn')
 elif (input_month == 'October') and (1 <= input_day <= 31):
@@ -35,5 +41,6 @@ elif (input_month == 'November') and (1 <= input_day <= 30):
     print('Autumn')
 elif (input_month == 'December') and (1 <= input_day <= 20):
     print('Autumn')
+#Check ì the string and int are valid (an actual month and day)
 else:
     print('Invalid')
